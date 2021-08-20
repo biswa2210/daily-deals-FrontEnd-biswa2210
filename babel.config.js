b0c0a59f4f29df0fa267f94b9cd9bf623e09c342
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-08-20 19:07:28 +0530

    added by biswa

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = function(api) {
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
