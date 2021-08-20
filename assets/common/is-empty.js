5ab496c4fa2a984870d7a2e8ef7f0b57a7429ff2
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-08-20 19:05:32 +0530

    added by biswa

--- assets/common/is-empty.js
+++ assets/common/is-empty.js
@@ -1,4 +1,10 @@
 const isEmpty = value =>
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
+
     value === undefined || 
     value === null ||
     (typeof value === "object" && Object.keys(value).length === 0) ||
